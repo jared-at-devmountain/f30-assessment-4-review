9963896aca796575026c1b14e2b31deb92cbf89f
Jared Collier <jared@Jareds-MBP.localdomain>
2022-10-21 15:39:24 -0600

    adds fortune feature

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 const complimentBtn = document.getElementById("complimentButton")
+const fortuneBtn = document.getElementById("fortuneButton")
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -8,4 +9,17 @@ const getCompliment = () => {
     });
 };
 
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+    .then((response) => {
+        const fortune = response.data
+        alert(fortune)
+    })
+    .catch((error) => {
+        console.log('You had an error! Behold: ')
+        console.log(error)
+    })
+}
+
 complimentBtn.addEventListener('click', getCompliment)
+fortuneBtn.addEventListener('click', getFortune)
--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,19 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
-    }
+    },
+    getFortune: (req, res) => {
+        const fortunes = [
+            'You will win a million dollars cash and get mugged, lose it all. Sorry.',
+            'You will be biking, crash into someone, and end up with their bag that had $1M cash... you try to return it, but the person is gone. Oh well.',
+            'You will donate a million dollars to your favorite charity, if you have a million dollars...',
+            'You find out your favorite charity is fraudulent.',
+            'You go to DevMountain to get a real job.'
+        ]
 
+        let randomIndex = Math.floor(Math.random() * fortunes.length)
+        let randomFortune = fortunes[randomIndex]
+
+        res.status(200).send(randomFortune)
+    }
 }
